f5091e67fda9f15a2261f21121449464890a2fcf
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-06-28 21:21:11 -0500

    fix

--- main.cs
+++ main.cs
@@ -10,20 +10,21 @@ public static void DumpTskFiles()
   var q =
     from x in GetTaskcoachXMLStreams()
     select x;
-    q.Dump();
+  q.Dump();
 }
 
 public static IEnumerable<XElement> GetTaskcoachXMLStreams()
 {
-	return from xml in GetTaskcoachXMLs()
-			   select XElement.Load(xml.FullName);
+  return from xml in GetTaskcoachXMLs()
+         select XElement.Load(xml.FullName);
 }
 
 // Name argument is case-insensitive.
-public static IEnumerable<FileInfo> GetTaskcoachXMLs()
+public static IEnumerable<FileInfo> GetTaskcoachXMLs(
+  string envVarName = "TASKCOACH_FP")
 {
   var TASKCOACH_XML_ENV =
-    Environment.GetEnvironmentVariable("TASKCOACH_FP");
+    Environment.GetEnvironmentVariable(envVarName);
   var q =
     from fp in TASKCOACH_XML_ENV.Split(
       new[] {Path.PathSeparator}, StringSplitOptions.None)
